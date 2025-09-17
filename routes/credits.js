ad8df2b3740d0da13976636be78ec7e4bd743ec0
Heather Eliezer <heather.glickman99@login.cuny.edu>
2025-09-17 12:17:58 -0400

    Add credits route

--- routes/credits.js
+++ routes/credits.js
@@ -1,2 +1,7 @@
-﻿const router = require('express').Router();
+﻿const router = require("express").Router();
+
+router.get("/land-use", (req, res) => {
+  res.json([]);
+});
+
 module.exports = router;
